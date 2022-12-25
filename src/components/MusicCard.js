58d6bf22277c35a3eb79e13b572ddd55609cf21d
BenHur Albertassi da Silva <benhuralber@yahoo.com.br>
2022-12-25 17:48:48 -0300

    FIX: checkbox custom

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -4,6 +4,8 @@ import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongs
 import getMusics from '../services/musicsAPI';
 import MsgLoading from './MsgLoading';
 
+import '../css/Album.css'
+
 class MusicCard extends React.Component {
   constructor(props) {
     super(props);
@@ -66,21 +68,23 @@ class MusicCard extends React.Component {
 
     return (
 
-      <div>
-
-        <p data-testid="album-name">{ cantor.collectionName }</p>
-        <p data-testid="artist-name">{ cantor.artistName }</p>
-
+      <div className='div-muisic-card'>
+        <div className='info-album-card'>
+          <p className="album-name-card">{ cantor.collectionName }</p>
+          <p className="artist-name-card">{ cantor.artistName }</p>
+        </div>
+        <div className='musica-player'>
         {
           musicas.length >= 1 && musicas.map((musica, indice) => (
             indice !== 0 && (
+              <div >
 
-              <div key={ musica.collectionId }>
+                <div className='div-musica' key={ musica.collectionId }>
 
-                <p>{musica.trackName}</p>
+                <p className='player-music'>{musica.trackName}</p>
 
                 <audio
-                  data-testid="audio-component"
+                    className="player-music"
                   src={ musica.previewUrl }
                   controls
                 >
@@ -91,20 +95,25 @@ class MusicCard extends React.Component {
                   <code>audio</code>
                 </audio>
 
-                <label htmlFor="boxzinho">
-                  Curtidas
-                  <input
+                  <label htmlFor={musica.trackId} className='boxzinho'>
+                  Amei
+                    <input
+                    className='player-music'
+                    // name='boxzinho'
                     type="checkbox"
                     name={ musica.trackId }
                     checked={ this.curtidinhas(musica.trackId) }
                     id={ musica.trackId }
-                    data-testid={ `checkbox-music-${musica.trackId}` }
                     onChange={ (evento) => this.addCurtidinhas(evento, musica) }
                   />
                 </label>
-              </div>)
-          ))
-        }
+              </div>
+                <br className='risco' />
+          </div>
+            )
+            ))
+          }
+          </div>
       </div>
     );
   }
@@ -115,45 +124,3 @@ MusicCard.propTypes = {
 };
 
 export default MusicCard;
-
-// Linha 7 definição do this.state, basiquinho como todos feits até aqui
-
-// Linha 11 esta buscando (ou linkando(não sei qual palavra é melhor para usar)) as props do buscaid
-
-// Linha 19 componentdidmounth eu tinha tentado fazer algo parecido em outra pagina ( Header.jsx (linha 117 explico melhor))
-// Linha 20 Desestruturação do { id }
-// Linha 21 chamada da função getFavoriteSongs dentro da constante Item que foi chamada mais na frente
-
-// Linha 26 vai buscar o array dentro do parametro que foi definido a cima em .then() -> ele retorna uma promisse.
-
-// Linha 32 função para adcionar aos favortiso, coloquei curtidas pq é o que eu uso no proprio spotify
-// Linha 33 definindo o caminho do checked, o botão que é definido mais a baixo
-// Linha 35 defino o estado
-// Linha 37 loading como true, para mais em abaixo entrar a chamada que vai mostrar a msg na tela
-// Linha 40 ainda dentro do setstate coloco uma arrowfunction async, nela entraremos com o IF
-// Linha 41 se o checked for marcado ele adiciona musica no favoritos
-// Linha 43 se o checked for desmarcado ele remove a musica do favoritor
-// Coloquei 'musicax' como parametro na linha 32 pq fiquei com medo de me confundir, deixei bem definido onde ela esta
-
-// Linha 55 pegando o id da Favoritas (Curtididas) para não favoritar muitas, apenas a selecionada (id)
-
-// Linha 60 Inicio do render
-
-// Linha 61 desestruturação
-// Linhas 63 e 64 Condição para carregar o component MsgLoading
-
-// Linha 69 Div pegando todo o return
-
-// Linha 73 joga na tela o nome do album
-// Linha 72 joga na tela o nome do cantor
-
-// Linha 75 Caso passe no requisito (>2) faz um .map das musicas
-// Linha 78 definição da key pelo collectionID (todo id é unico)
-
-// Linha 80 Nome da musica em tela
-
-// Linha 82 requisito 7 ja veio pronto
-
-// Linha 94 imput do checkbox das favoritas
-
-// Linha 98 a 101 vai fazer o mapeamento pelo trackID
